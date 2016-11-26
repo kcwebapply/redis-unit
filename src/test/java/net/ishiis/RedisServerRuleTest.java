21ff3f5226248511bafd8803afab0052d5b75d0b
ishiis <ishii.shunichi@gmail.com>
2016-11-26 16:57:21 +0900

    add Redis test class

---
+++ src/test/java/net/ishiis/RedisServerRuleTest.java
@@ -0,0 +1,17 @@
+package net.ishiis;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class RedisServerRuleTest {
+    @Test
+    public void testBefore() {
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void testAfter() {
+        Assert.assertTrue(true);
+    }
+}
---
+++ src/test/java/net/ishiis/RedisServerTest.java
@@ -0,0 +1,18 @@
+package net.ishiis;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class RedisServerTest {
+    @Test
+    public void testStart() {
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void testStop() {
+        Assert.assertTrue(true);
+    }
+
+}
