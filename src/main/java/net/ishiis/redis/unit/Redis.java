513b21abbd19c2c55cd96c5a4ac2262481a7b3cd
ishiis <ishii.shunichi@gmail.com>
2016-12-01 01:26:24 +0900

    update redis server

--- src/main/java/net/ishiis/redis/unit/Redis.java
+++ src/main/java/net/ishiis/redis/unit/Redis.java
@@ -1,23 +1,7 @@
 package net.ishiis.redis.unit;
 
-import java.io.UnsupportedEncodingException;
-import java.net.URLDecoder;
-
-public abstract class Redis {
-    private static final String DEFAULT_REDIS_BINARY_NAME = "redis-server.3.2.5";
-
-    abstract public void start();
-    abstract public void stop();
-    abstract public Boolean isAlive();
-
-    protected String getDefaultRedisBinaryPath() {
-        String redisBinaryPath;
-        try {
-            redisBinaryPath = URLDecoder.decode(
-                    this.getClass().getClassLoader().getResource(DEFAULT_REDIS_BINARY_NAME).getPath(), "UTF-8");
-        } catch (UnsupportedEncodingException e) {
-            throw new RuntimeException("UTF-8 is not supported Encoding.", e);
-        }
-        return redisBinaryPath;
-    }
+public interface Redis {
+    void start();
+    void stop();
+    Boolean isAlive();
 }
--- src/main/java/net/ishiis/redis/unit/RedisServer.java
+++ src/main/java/net/ishiis/redis/unit/RedisServer.java
@@ -2,73 +2,58 @@ package net.ishiis.redis.unit;
 
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.util.Arrays;
-import java.util.List;
-import java.util.UUID;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
-public class RedisServer extends Redis {
+public class RedisServer implements Redis {
     public static final Integer DEFAULT_REDIS_SERVER_PORT = 6379;
 
     private static final String REDIS_START_UP_COMPLETE_MESSAGE = "The server is now ready to accept connections on port";
     private static final String REDIS_ADDRESS_ALREADY_IN_USE = "Address already in use";
 
-    private List<String> command;
+    private RedisConfig config;
     private Process process;
-    private Integer port;
-    private String logFileName;
 
     public RedisServer() {
         this(DEFAULT_REDIS_SERVER_PORT);
     }
 
     public RedisServer(Integer port) {
-        this.port = port;
-        this.logFileName = port + "_" + UUID.randomUUID().toString() + ".log";
-        this.command = Arrays.asList(getDefaultRedisBinaryPath(),
-                "--port", Integer.toString(port), "--logfile", logFileName);
+        this(new RedisConfig.ServerBuilder(port).build());
     }
 
-    public RedisServer(String redisBinaryPath) {
-        this(redisBinaryPath, DEFAULT_REDIS_SERVER_PORT);
-    }
-
-    public RedisServer(String redisBinaryPath, Integer port) {
-        this.port = port;
-        this.command = Arrays.asList(redisBinaryPath, "--port", Integer.toString(port));
-    }
-
-    public RedisServer(List<String> command) {
-        this.command = command;
+    public RedisServer(RedisConfig config) {
+        this.config = config;
     }
 
     @Override
     public void start() {
-        // Create Redis working directory.
-        File tempDirectory;
+        // Create Redis working directory and empty config file.
+        Path tempDirectory;
         try {
-            tempDirectory = Files.createTempDirectory("redis-unit").toFile();
+            tempDirectory = Files.createTempDirectory("redis-unit-");
+            Paths.get(tempDirectory.toString(), config.getConfigFile().toString()).toFile().createNewFile();
         } catch (IOException e) {
-            throw new RuntimeException("Unable to create a directory.", e);
+            throw new RuntimeException("Unable to create a temp file.", e);
         }
 
-        File redisBinary = new File(command.get(0));
-        redisBinary.setExecutable(true);
+        Path redisBinary = Paths.get(config.getRedisBinaryPath());
+        redisBinary.toFile().setExecutable(true);
 
         // Start redis server.
-        ProcessBuilder pb = new ProcessBuilder(command);
-        pb.directory(tempDirectory);
+        ProcessBuilder processBuilder = new ProcessBuilder(config.getCommand());
+        processBuilder.directory(tempDirectory.toFile());
         try {
-            process = pb.start();
+            process = processBuilder.start();
         } catch (IOException e) {
             throw new RuntimeException("Unable to start Redis.", e);
         }
 
-        File logFile = new File(tempDirectory.getPath(), logFileName);
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(logFile))) {
+        Path logFile = Paths.get(tempDirectory.toString(), config.getLogFile().toString());
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(logFile.toFile()))) {
             String outputLine;
             do {
                 outputLine = bufferedReader.readLine();
@@ -99,10 +84,6 @@ public class RedisServer extends Redis {
     @Override
     public Boolean isAlive() {
         return process != null && process.isAlive();
-    }
-
-    public Integer getPort() {
-        return port;
     }
 
 }
