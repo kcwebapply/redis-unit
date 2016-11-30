8015c72f0b6b7ac80bd2ff7c83635798cb71aa18
ishiis <ishii.shunichi@gmail.com>
2016-12-01 01:45:31 +0900

    add redis master slave class

---
+++ src/main/java/net/ishiis/redis/unit/RedisMasterSlave.java
@@ -0,0 +1,47 @@
+package net.ishiis.redis.unit;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static net.ishiis.redis.unit.RedisServer.DEFAULT_REDIS_SERVER_PORT;
+
+public class RedisMasterSlave implements Redis {
+    private final RedisServer master;
+    private final List<RedisServer> slaves = new ArrayList<>();
+
+    public RedisMasterSlave() {
+        this(DEFAULT_REDIS_SERVER_PORT, DEFAULT_REDIS_SERVER_PORT + 1);
+    }
+
+    public RedisMasterSlave(Integer masterPort, Integer... slavePorts) {
+        this(new RedisConfig.ServerBuilder(masterPort).build(),
+                Arrays.stream(slavePorts)
+                        .map(slavePort -> new RedisConfig.ServerBuilder(slavePort).masterPort(masterPort).build())
+                        .collect(Collectors.toList()));
+    }
+
+    public RedisMasterSlave(RedisConfig masterConfig, List<RedisConfig> slaveConfigs) {
+        master = new RedisServer(masterConfig);
+        slaveConfigs.forEach(slaveConfig -> slaves.add(new RedisServer(slaveConfig)));
+    }
+
+    @Override
+    public void start() {
+        master.start();
+        slaves.forEach(RedisServer::start);
+    }
+
+    @Override
+    public void stop() {
+        master.stop();
+        slaves.forEach(RedisServer::stop);
+    }
+
+    @Override
+    public Boolean isAlive() {
+        return master != null && slaves.isEmpty() && master.isAlive() && slaves.stream().allMatch(RedisServer::isAlive);
+    }
+
+}
---
+++ src/test/java/net/ishiis/redis/unit/RedisMasterSlaveTest.java
@@ -0,0 +1,29 @@
+package net.ishiis.redis.unit;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+import redis.clients.jedis.Jedis;
+
+import static net.ishiis.redis.unit.RedisServer.DEFAULT_REDIS_SERVER_PORT;
+
+public class RedisMasterSlaveTest {
+
+    @Test
+    public void testStart() {
+        RedisMasterSlave redisMasterSlave = new RedisMasterSlave();
+        redisMasterSlave.start();
+
+        Jedis master = new Jedis("localhost", DEFAULT_REDIS_SERVER_PORT);
+        Assert.assertTrue(master.info("Replication").contains("role:master"));
+        master.close();
+
+        Jedis slave = new Jedis("localhost", DEFAULT_REDIS_SERVER_PORT + 1);
+        Assert.assertTrue(slave.info("Replication").contains("role:slave"));
+        Assert.assertTrue(slave.info("Replication").contains("master_host:127.0.0.1"));
+        Assert.assertTrue(slave.info("Replication").contains("master_port:6379"));
+        slave.close();
+
+        redisMasterSlave.stop();
+    }
+}
